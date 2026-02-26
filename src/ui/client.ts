77f24e1bf9430af5cc37075d0dde58492801d9ab
yiling ji <njjyl723@gmail.com>
2026-02-27 00:03:45 +0800

    revert ui client change

--- src/ui/client.ts
+++ src/ui/client.ts
@@ -1,6 +1,4 @@
 import { hc } from 'hono/client'
 import type { AppType } from '../index'
 
-// Use absolute URL to avoid issues with relative paths in some environments
-const origin = typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000'
-export const client = hc<AppType>(`${origin}/api`)
+export const client = hc<AppType>('/api')
